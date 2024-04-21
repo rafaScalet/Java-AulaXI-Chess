ab88ec2ecffed2937c2ad64140b8ca11d5add301
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 18:58:44 -0300

    completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,25 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
+        Chess[][] ChessBoard = new Chess[8][8];
+        boolean squaresColor = false;
 
+        for (int row = 0; row < ChessBoard[0].length; row++) {
+            squaresColor = !squaresColor;
+            for (int col = 0; col < ChessBoard[0].length; col++) {
+                ChessBoard[row][col] = new Chess();
+                ChessBoard[row][col].setLine(row);
+                ChessBoard[row][col].setColumn(col);
+                ChessBoard[row][col].setPiece("vazio");
+                if (squaresColor) {
+                    ChessBoard[row][col].setColor("Branca");
+                    squaresColor = false;
+                } else {
+                    ChessBoard[row][col].setColor("Preta");
+                    squaresColor = true;
+                }
+            }
+        }
     }
 
     static class Chess {
