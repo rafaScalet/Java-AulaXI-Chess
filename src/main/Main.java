3d7b94c5a56ecbf67f44b3857b1b8670b63c2470
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 18:56:11 -0300

    created 'chess' class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,4 +4,43 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    static class Chess {
+        private int line;
+        private int column;
+        private String piece;
+        private String color;
+
+        public int getLine() {
+            return line;
+        }
+
+        public void setLine(int line) {
+            this.line = line;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        public void setColumn(int column) {
+            this.column = column;
+        }
+
+        public String getPiece() {
+            return piece;
+        }
+
+        public void setPiece(String piece) {
+            this.piece = piece;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+    }
 }
